f92cb0a9cb2716590b9ca89fd6efb6ec068490c1
Валерий <94367062+gugutsidze-vv@users.noreply.github.com>
2024-04-29 22:29:42 +0300

    Added deploy function Added access to postgresql in settings.py

--- models/settings.py
+++ models/settings.py
@@ -69,11 +69,11 @@ WSGI_APPLICATION = 'models.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': '5432',
     }
 }
 
