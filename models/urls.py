f19cf452f71ebfba894f6ae1b4f97c31c9f38e1e
Валерий <94367062+gugutsidze-vv@users.noreply.github.com>
2024-05-01 23:14:18 +0300

    Added djoser added api app added djoser auth urls

--- models/urls.py
+++ models/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('projects/', include('projects.urls')),
 ]
