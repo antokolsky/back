5bac9b892f510b829b8464b0d53d28c5fb7bf487
Валерий <94367062+gugutsidze-vv@users.noreply.github.com>
2024-05-05 21:06:22 +0300

    Added Users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,81 @@
-from django.db import models
+"""Модели связанные с пользователем"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+EMAIL_LENGTH = 254
+NAMES_LENGTH = 100
+
+
+class Country(models.Model):
+    """Класс Стран"""
+    name_ru = models.CharField(
+        'Название Русское',
+        max_length=NAMES_LENGTH,
+        blank=False,
+        unique=True
+    )
+    name_en = models.CharField(
+        'Название Английское',
+        max_length=NAMES_LENGTH,
+        blank=False,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Страна'
+        verbose_name_plural = 'Страны'
+        ordering = ('name_ru',)
+
+    def __str__(self) -> str:
+        return f'{self.name_ru}'
+
+
+class City(models.Model):
+    """Класс городов"""
+    name_ru = models.CharField(
+        'Название Русское',
+        max_length=NAMES_LENGTH,
+        blank=False
+    )
+    name_en = models.CharField(
+        'Название Английское',
+        max_length=NAMES_LENGTH,
+        blank=False
+    )
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE,
+        related_name='cities',
+        verbose_name='Город'
+    )
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ('name_ru',)
+
+    def __str__(self) -> str:
+        return f'{self.country.name_ru} - {self.name_ru}'
+
+
+class User(AbstractUser):
+    """Класс описания пользователя"""
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=EMAIL_LENGTH,
+        unique=True
+    )
+    first_name = models.CharField('Имя', max_length=NAMES_LENGTH, blank=False)
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=NAMES_LENGTH,
+        blank=False
+    )
+    phone_number = models.CharField(
+        'Номер телефона',
+        max_length=11,
+        blank=True
+    )
