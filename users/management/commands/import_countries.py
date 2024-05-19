d7e5a29f94625fc61c360b43a57f615bbd494b19
valery <gugutsidze_vv@rosakhutor.com>
2024-05-19 20:54:39 +0300

    Rewritten synchrony get_countries

--- users/management/commands/import_countries.py
+++ users/management/commands/import_countries.py
@@ -1,13 +1,42 @@
+import asyncio
 import os
 from http import HTTPStatus
 
-import requests
+import aiohttp
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
 from dotenv import load_dotenv
 
 from users.models import Country
 
 load_dotenv()
+
+
+async def get_countries(params):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                f'{os.getenv("GEOHELPER_URL")}/api/v1/countries',
+                params=params
+        ) as response:
+            if response.status == HTTPStatus.OK:
+                response = await response.json()
+                if response['result']:
+                    if response['success']:
+                        await Country.objects.abulk_create(
+                            [
+                                Country(
+                                    name_ru=country['name'],
+                                    name_en=country['localizedNames']['en']
+                                ) for country in response['result']
+                            ]
+                        )
+                    else:
+                        print(
+                            'Статус ответа сервера отрицательный. '
+                            'Проверьте ключ'
+                        )
+                else:
+                    print('В ответе не найдены данные о странах')
 
 
 class Command(BaseCommand):
@@ -21,30 +50,15 @@ class Command(BaseCommand):
     adding_message_text: str = 'Добавляем страны'
 
     def handle(self, *args, **options):
-        with requests.session() as session:
-            with session.get(
-                f'{os.getenv("GEOHELPER_URL")}/api/v1/countries',
-                params=self.parameters
-            ) as response:
-                if response.status_code == HTTPStatus.OK:
-                    response = response.json()
-                    if response['success']:
-                        if Country.objects.count() > 0:
-                            print(self.clear_message_text)
-                            count = Country.objects.count()
-                            Country.objects.all().delete()
-                            print(f'{count} записей удалены')
-                        print(self.adding_message_text)
-                        Country.objects.bulk_create(
-                            [
-                                Country(
-                                    name_ru=country['name'],
-                                    name_en=country['localizedNames']['en']
-                                )
-                                for country in response['result']
-                            ]
-                        )
-                        print(f'{Country.objects.count()} записей добавлено')
-                else:
-                    print(response.status_code)
-        
+        try:
+            if Country.objects.last():
+                print(
+                    'В таблице уже есть данные. '
+                    'Очистите таблицу вручную с очисткой primary key'
+                )
+            else:
+                asyncio.get_event_loop().run_until_complete(
+                    get_countries(self.parameters)
+                )
+        except OperationalError as e:
+            print(f'{e}. Не проведены миграции!')
