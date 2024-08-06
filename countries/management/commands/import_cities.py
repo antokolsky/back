96316c06e902fa06072b201da3699978f9a7517d
gugutsidze-vv <gugutsidze-vv@rosakhutor.com>
2024-08-06 20:45:38 +0300

    cities edit

--- countries/management/commands/import_cities.py
+++ countries/management/commands/import_cities.py
@@ -1,38 +1,41 @@
 import os
-from dotenv import load_dotenv
-from django.core.management import BaseCommand
-from users.models import City, Country
-import asyncio
-from django.db.utils import OperationalError
-import aiohttp
 import copy
-from tqdm.asyncio import tqdm, trange
+import asyncio
+
+from dotenv import load_dotenv
+import aiohttp
+from tqdm.asyncio import trange
+from django.core.management import BaseCommand
+from django.db.utils import OperationalError
+
+from countries.models import Country, City
 
 load_dotenv()
+
+
 async def async_generator(limit):
-    for i in range(1, limit + 1):
-        yield i
+    for worker in range(1, limit + 1):
+        yield worker
+
 
 async def get_cities():
-
     connection = aiohttp.TCPConnector(limit=9)
     timeout = aiohttp.ClientTimeout(total=300)
-    parameters: dict = {
+    parameters = {
         'apiKey': os.getenv('GEOHELPER_KEY'),
         'locale[lang]': os.getenv('GEOHELPER_LANG'),
         'locale[fallbackLang]': os.getenv('GEOHELPER_LANG'),
-        'pagination[limit]': 100
+        'pagination[limit]': 100,
     }
     async with aiohttp.ClientSession(
-            timeout=timeout,
-            connector=connection
+        timeout=timeout, connector=connection
     ) as session:
         async for country in Country.objects.values('iso', 'name_ru'):
             parameters['filter[countryIso]'] = country['iso']
             print(country['name_ru'])
             async with session.get(
                 f'{os.getenv("GEOHELPER_URL")}/api/v1/cities',
-                params=parameters
+                params=parameters,
             ) as response:
                 response = await response.json()
                 pages = response.get('pagination').get('totalPageCount')
@@ -44,8 +47,8 @@ async def get_cities():
                 async for page in trange(1, pages + 1):
                     parameters_page['pagination[page]'] = page
                     async with session.get(
-                            f'{os.getenv("GEOHELPER_URL")}/api/v1/cities',
-                            params=parameters_page
+                        f'{os.getenv("GEOHELPER_URL")}/api/v1/cities',
+                        params=parameters_page,
                     ) as response_page:
                         response_page_answer = await response_page.json()
                         if response_page_answer['success']:
@@ -58,16 +61,15 @@ async def get_cities():
                 await City.objects.abulk_create(
                     [
                         City(
-                            name_ru=city_ru,
-                            name_en=city_en,
-                            country=country
-                        ) for city_ru, city_en in all_cities.items()
+                            name_ru=city_ru, name_en=city_en, country=country
+                        )
+                        for city_ru, city_en in all_cities.items()
                     ]
                 )
 
 
 class Command(BaseCommand):
-    help: str = 'Import cities by countries from geohelper.info'
+    help = 'Import cities'
 
     def handle(self, *args, **options):
         try:
