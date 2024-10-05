6639a6539cbb7c91315c6e210c5f6fc7caa257e7
gugutsidze-vv <gugutsidze-vv@rosakhutor.com>
2024-10-06 00:16:57 +0300

    fixing cors

--- models/urls.py
+++ models/urls.py
@@ -10,6 +10,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
         default_version="v1",
+        url='http://antokolsky.ddns.net/',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
